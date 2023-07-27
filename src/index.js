b500d3182174a5655f63c7de33b0e506f4b25fea
NikhilBabu829 <nikhilbabu829@gmail.com>
2023-07-28 01:13:02 +0530

    test2

--- src/index.js
+++ src/index.js
@@ -14,8 +14,24 @@ const somethingAgain = {
     date : []
 }
 
+
+
 localStorage.setItem("project", JSON.stringify(template))
 const allData = JSON.parse(localStorage.getItem('project'))
 allData.project[0].name = "abcd"
 allData.project.push(somethingAgain)
 localStorage.setItem("project", JSON.stringify(allData))
+
+const button = document.querySelector(".btnTest")
+
+button.addEventListener("click",()=>{
+    const somethingAgain1 = {
+        name : "2123",
+        task : [],
+        date : []
+    }
+    const allData = JSON.parse(localStorage.getItem('project'))
+    allData.project.push(somethingAgain1)
+    localStorage.setItem("project", JSON.stringify(allData))
+})
+
