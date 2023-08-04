f8e876e76010d8438358e5ed78fce79aa7028812
NikhilBabu829 <nikhilbabu829@gmail.com>
2023-08-04 18:11:09 +0530

    test10

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const form = document.querySelector('.formTest');    
-const selectedValue = document.querySelectorAll('input[name="radioBtn"]');
+const formToCreateNewProject = document.querySelector('.formToCreateNewProject')
+const newProject = document.querySelector('.newProject');
+const projectMenu = document.querySelector('.projectMenu');
+let selectedValue = document.querySelectorAll('.radioBtn');
 
 const radioValue = {
     projectName : 'index'
@@ -40,11 +43,32 @@ function someFunction(userName, password) {
     }
 }
 
-selectedValue.forEach((ele)=>{
-    ele.addEventListener('change',()=>{ 
-        radioValue.projectName = ele.value
-    }) 
-})
+function createRadioButton(nameOfTheProject){
+    const labelForRadioBtn = document.createElement('label')
+    labelForRadioBtn.setAttribute('for', nameOfTheProject)
+    labelForRadioBtn.innerText = nameOfTheProject
+    const radioBtn = document.createElement("input");
+    radioBtn.setAttribute("type","radio")
+    radioBtn.setAttribute("name","radioBtn")
+    radioBtn.setAttribute("id",nameOfTheProject)
+    radioBtn.setAttribute("value",nameOfTheProject)
+    radioBtn.classList.add("radioBtn");
+    projectMenu.append(labelForRadioBtn)
+    projectMenu.append(radioBtn)
+    selectedValue = document.querySelectorAll('.radioBtn');
+    radio(selectedValue)
+}
+function radio(selectedValue){
+    selectedValue.forEach((ele)=>{
+        ele.addEventListener("click",()=>{
+            if(ele.checked){
+                radioValue.projectName = ele.value
+                console.log(radioValue.projectName)
+            }
+        })
+    })
+}
+
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
@@ -52,3 +76,21 @@ form.addEventListener('submit',(e)=>{
     const password = document.querySelector('.password');
     someFunction(userName.value, password.value);
 })
+
+newProject.addEventListener('click',()=>{
+    formToCreateNewProject.style.visibility = 'visible';
+})
+
+formToCreateNewProject.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const projectName = document.querySelector('.projectName');
+    const templateForNewproject = {
+        project : projectName.value,
+        tasks : [],
+    }
+    const dataInLocalStorage = JSON.parse(localStorage.getItem('data'))
+    dataInLocalStorage.project[dataInLocalStorage.project.length] = templateForNewproject
+    localStorage.setItem('data', JSON.stringify(dataInLocalStorage));
+    createRadioButton(projectName.value)
+    formToCreateNewProject.style.visibility = 'hidden';
+});
